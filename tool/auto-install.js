ef9199070833c0dc9d78fb3b354d143a1ef3eb57
waonpad <waonpad@gmail.com>
2023-10-10 06:42:17 +0900

    fix(なし): 🐞 自動installが動くように修正

--- tool/auto-install.js
+++ tool/auto-install.js
@@ -6,11 +6,16 @@ const installCommand = 'yarn install --frozen-lockfile';
 
 const main = () => {
   try {
-    const stdout = execSync("git diff 'HEAD@{1}' --name-only").toString('utf-8');
+    const stdout = execSync('git diff HEAD@{1} --name-only').toString('utf-8');
+
+    // console.log(stdout);
+
     const changedFiles = stdout
       .trim()
       .split('\n')
-      .filter((name) => targetFiles.includes(name));
+      .filter((name) => targetFiles.includes(name.split('/').pop()));
+
+    // console.log(changedFiles);
 
     if (changedFiles.length > 0) {
       execSync(installCommand, { stdio: 'inherit' });
